dda3203c4b4ea02780ad5ba6fe3db49008c8c412
FruitRiin <nassii74@gmail.com>
2023-05-30 00:52:55 +0900

    スタイル調整

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -3,10 +3,12 @@ import Versions from './components/Versions.vue'
 </script>
 
 <template>
-  <div  style="display: flex;">
-    <button class="ui button task" @click="acticveWindow( win)" v-for="win in filteredWindows" :key="win.kCGWindowOwnerPID + win.kCGWindowNumber">
+  <div class="container is-fluid has-text-white has-background-dark" style="height: 100%">
+
+  <div  style="display: flex;" class="">
+    <button class="button task is-dark" @click="acticveWindow( win)" v-for="win in filteredWindows" :key="win.kCGWindowOwnerPID + win.kCGWindowNumber">
       <div v-if="win.kCGWindowName" >{{win.kCGWindowName}}</div>
-      <div v-if="win.kCGWindowName"> - </div>
+      <div v-if="win.kCGWindowName">&nbsp;-&nbsp;</div>
       <div>{{win.kCGWindowOwnerName}}</div>
     </button>
   </div>
@@ -18,6 +20,7 @@ import Versions from './components/Versions.vue'
       <th>WindowLayer</th>
       <th>WindowName</th>
       <th> WindowOnScreen</th>
+      <th>kCGWindowSharingState</th>
     </tr>
     <tr v-for="win in filteredWindows" @click="acticveWindow( win)">
       <td >{{win.kCGWindowOwnerName}}</td>
@@ -26,22 +29,26 @@ import Versions from './components/Versions.vue'
       <td>{{ win.kCGWindowLayer }}</td>
       <td>{{win.kCGWindowName}}</td>
       <td>{{win.kCGWindowIsOnscreen}}</td>
+      <td>{{win.kCGWindowSharingState}}</td>
 
     </tr>
   </table>
   <Versions></Versions>
+  </div>
+
 </template>
 
 
 <script lang="ts">
 import { Window } from "../../type";
+import { defineComponent } from "vue";
 declare global {
   interface Window {
     electronAPI: any;
   }
 }
 
-export default {
+export default defineComponent({
   data(){
     return {
       windows: null as Window[] | null
@@ -49,7 +56,6 @@ export default {
   },
   mounted() {
     window.electronAPI.process((event, value) => {
-      console.log(value)
       this.windows = JSON.parse(value)
     })
   },
@@ -64,18 +70,50 @@ export default {
         if(win.kCGWindowName === "Item-0") return false
         if(win.kCGWindowOwnerName === "Window Server") return false
         if(win.kCGWindowOwnerName === "コントロールセンター") return false
+        if(win.kCGWindowOwnerName === "Notification Center") return false
         if(win.kCGWindowName === "Spotlight") return false
         if(win.kCGWindowOwnerName === "GoogleJapaneseInputRenderer") return false
+        if(win.kCGWindowOwnerName === "taskbar") return false
         return  true
       }).sort((win1, win2) => {
         return win1.kCGWindowOwnerPID - win2.kCGWindowOwnerPID
       })
     }
   }
-}
+})
 </script>
 
-<style lang="sass">
-@import "bulma/bulma"
+<style lang="scss">
+@import "bulma/bulma";
+
+.button.is-dark {
+  border: solid 2px;
+  border-color: $grey-light !important;
+  border-radius: 4px;
+}
+.button {
+  margin: 8px 3px;
+}
+
+.task {
+  width: 200px;
+  display: inline-flex;
+  justify-content: left;
+  white-space: initial;
+
+
+
+  div {
+    width: fit-content;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+    word-break:break-all
+  }
+}
+
+
 
 </style>
+
