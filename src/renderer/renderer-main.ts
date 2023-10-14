65831a3ef41a9812e4de40fb0c89aa3daa273156
FruitRiin <nassii74@gmail.com>
2023-10-14 17:30:24 +0900

    fix?

--- src/renderer/renderer-main.ts
+++ src/renderer/renderer-main.ts
@@ -4,9 +4,8 @@ import App from './src/App.vue'
 import { routes } from 'vue-router/auto/routes'
 
 // @ts-ignore
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter } from 'vue-router/auto'
 const router = createRouter({
-  history: createWebHistory('#'),
   routes
 })
 
